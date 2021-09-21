848ea7455a4150cb1759646f12c209125e610921
Stefan O'Neil <oneil.stefan@gmail.com>
2021-09-21 15:45:07 +0900

    Created pages.

---
+++ script.js
@@ -0,0 +1,16 @@
+// Code to control expanding panels
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
